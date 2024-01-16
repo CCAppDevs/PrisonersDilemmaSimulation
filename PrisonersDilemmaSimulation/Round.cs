7b3d96a2c37b9d213b2ef4cd8af05eeaa594828e
Jesse Harlan <jesse.harlan@centralia.edu>
2024-01-16 11:44:08 -0800

    Add project files.

---
+++ PrisonersDilemmaSimulation/Round.cs
@@ -0,0 +1,27 @@
+﻿using PrisonersDilemmaSimulation.Strategies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrisonersDilemmaSimulation
+{
+    public class Round
+    {
+        public int RoundNumber { get; set; }
+        public IStrategy Player1 { get; set; }
+        public IStrategy Player2 { get; set; }
+        public bool Player1Defect { get; set; }
+        public bool Player2Defect { get; set; }
+        public int Player1ScoreForRound() {  return 0; }
+        public int Player2ScoreForRound() {  return 0; }
+
+        public Round(int round, IStrategy player1, IStrategy player2)
+        {
+            RoundNumber = round;
+            Player1 = player1;
+            Player2 = player2;
+        }
+    }
+}
---
+++ PrisonersDilemmaSimulation/Simulation.cs
@@ -0,0 +1,43 @@
+﻿using PrisonersDilemmaSimulation.Strategies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrisonersDilemmaSimulation
+{
+    public class Simulation
+    {
+        // TODO: Add code to run a simulation
+
+        // 100 simulations per pair of players (should be variable for maximum effect)
+
+        // scoring: Lowest Score Wins
+            // Two Players, each can either snitch or remain silent
+            // if both remain silent = both gain 1
+            // if one party snitches = 5 for the silent, 0 for the snitch
+            // if both snitch        = 3 for each
+
+        public Simulation()
+        {
+        
+        }
+
+        public void RunSimulation(IStrategy player1, IStrategy player2, int numberOfRounds)
+        {
+            //int player1Score = 0;
+            //int player2Score = 0;
+            List<Round> rounds = new List<Round>();
+
+            for (int i = 0; i < numberOfRounds; i++)
+            {
+                // get choices for each player
+                // record the round in the round array
+                rounds.Add(new Round(i, player1, player2));
+            }
+
+        }
+    }
+}
