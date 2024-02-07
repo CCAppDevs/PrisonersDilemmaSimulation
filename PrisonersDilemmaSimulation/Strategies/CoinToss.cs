4c4281fc95cceb5ee1a7d519cbdac0bf35f6efcd
martijor000 <82613327+martijor000@users.noreply.github.com>
2024-02-07 14:40:24 -0800

    Updated Deception and Added New Strategy CoinToss. Both of them work and are functional.

---
+++ PrisonersDilemmaSimulation/Strategies/CoinToss.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrisonersDilemmaSimulation.Strategies
+{
+    //Jordan's Second Strategy
+    public class CoinToss : AbstractStrategy
+    {
+        private decimal CountDefects;
+        private decimal CountMatches = 0;
+        private decimal Probability;
+        public override string GetName()
+        {
+            return "CoinToss";
+        }
+
+        public override void Notify(Match match)
+        {
+            CountDefects = match.Results.Count(T => T.Guid != Guid && T.TossResult == Result.Defect);
+        }
+
+        public override Result Play(IStrategy opponent)
+        {
+            if (CountMatches > 0)
+            {
+                Probability = CountDefects / CountMatches;
+
+                if(Probability < 0.50M)
+                {
+                    CountMatches++;
+                    return Result.Cooperate;
+                }
+                else
+                {
+
+                    CountMatches++;
+                    return Result.Defect;
+                }
+            }
+            else
+            {
+                CountMatches++;
+                return Result.Cooperate;
+            }
+        }
+
+        public override void ResetStrategy()
+        {
+            CountDefects = 0;
+            CountMatches = 0;
+            Probability = 0;
+        }
+    }
+}
