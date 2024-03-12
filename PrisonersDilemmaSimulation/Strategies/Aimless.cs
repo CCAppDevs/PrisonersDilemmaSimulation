7ec7d71c70f5f0a5b4c542053fb224e82598c5b1
Keri <79213053+KeriGabriel@users.noreply.github.com>
2024-03-11 17:50:43 -0700

    Keri Gabriel Individual Strategies

---
+++ PrisonersDilemmaSimulation/Strategies/Aimless.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrisonersDilemmaSimulation.Strategies
+{
+    internal class Aimless : AbstractStrategy
+    {
+        public override string GetName()
+        {
+            return "Aimless";
+        }
+        public override void Notify(Match match)
+        {
+            throw new NotImplementedException();
+        }
+        public override Result Play(IStrategy opponent)
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(1,50);
+            if (num <= 36)
+            {
+                return Result.Cooperate;
+            }
+            else
+            {
+                return Result.Defect;
+            }
+        }
+        public override void ResetStrategy()
+        {
+            
+        }
+    }
+}
---
+++ PrisonersDilemmaSimulation/Strategies/Insincerity.cs
@@ -0,0 +1,45 @@
+﻿using PrisonersDilemmaSimulation.Strategies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrisonersDilemmaSimulation.Strategies
+{
+    public class Insincerity : AbstractStrategy  //Keri
+    {
+        public int _count = 1;
+        public override string GetName()
+        {
+            return "insincerity";
+        }
+        public override void Notify(Match match)
+        {
+            _count++;
+        }
+        public override Result Play(IStrategy opponent)
+        {
+            if (_count % 5 == 0)
+            {
+                return Result.Defect;
+            }
+            if (_count % 13 == 0)
+            {
+                return Result.Defect;
+            }
+            else
+            {
+                return Result.Cooperate;
+            }
+        }
+
+        public override void ResetStrategy()
+        {
+            _count = 1;
+        }
+    }
+}
+
+       
+
